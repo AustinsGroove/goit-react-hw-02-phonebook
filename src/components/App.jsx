081621dd0d9bc6676829b0b07616e67f6a6182a8
AustinsGroove <vadimgajiy@gmail.com>
2023-12-03 00:35:58 +0200

    gogoo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+//
+import InputForm from './InputForm/InputForm';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter/Filter';
+//
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = contact => {
+    this.setState(({ contacts: prevContacts }) => {
+      const newContact = {
+        id: nanoid(),
+        name: contact.name,
+        number: contact.number,
+      };
+      return {
+        [`contacts`]: [...prevContacts, newContact],
+      };
+    });
+  };
+
+  removeContact = ({ target }) => {
+    const removableId = target.id;
+    const contacts = this.state.contacts;
+    this.setState({
+      contacts: contacts.filter(contact => contact.id !== removableId),
+    });
+  };
+
+  filterChange = ({ target: { value } }) => {
+    this.setState({
+      filter: value,
+    });
+  };
+
+  renderFilteredContacts = () => {
+    const contacts = this.state.contacts;
+    const filter = this.state.filter;
+    if (!filter) {
+      return contacts;
+    } else {
+      return contacts.filter(({ name }) =>
+        name.toLowerCase().includes(filter.toLocaleLowerCase())
+      );
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <InputForm
+          addContact={this.addContact}
+          contacts={this.state.contacts}
+        />
+        <h2>Contacts</h2>
+        <Filter filterChange={this.filterChange} value={this.state.filter} />
+        <ContactsList
+          contacts={this.renderFilteredContacts()}
+          removeContact={this.removeContact}
+        />
+      </>
+    );
+  }
+}
